f66aa4459293eb1a4691a9aaab96041466328a94
moskvinspace <me@moskvin.space>
2024-11-02 17:44:27 +0200

    add: log for storage

--- internal/storage/sqlite/sqlite.go
+++ internal/storage/sqlite/sqlite.go
@@ -3,23 +3,45 @@ package sqlite
 import (
 	"database/sql"
 	"fmt"
+	"log/slog"
 )
 
 type Storage struct {
-	db *sql.DB
+	log *slog.Logger
+	db  *sql.DB
 }
 
-func New(storagePath string) (*Storage, error) {
-	const op = "storage.sqlite.New"
+func New(log *slog.Logger, storagePath string) (*Storage, error) {
+	const operation = "storage.sqlite.New"
+
+	log = log.With(
+		slog.String("operation", operation),
+	)
 
 	db, err := sql.Open("sqlite3", storagePath)
 	if err != nil {
-		return nil, fmt.Errorf("%s: %w", op, err)
+		return nil, fmt.Errorf("%s: %w", operation, err)
 	}
 
-	return &Storage{db: db}, nil
+	log.Info("database opened")
+
+	return &Storage{
+		log: log,
+		db:  db,
+	}, nil
 }
 
-func (s *Storage) Stop() error {
-	return s.db.Close()
+func (s *Storage) Stop() {
+	const operation = "storage.sqlite.Stop"
+
+	s.log.With(
+		slog.String("operation", operation),
+	)
+
+	if err := s.db.Close(); err != nil {
+		s.log.Warn("close database failed")
+		return
+	}
+
+	s.log.Info("database closed")
 }
